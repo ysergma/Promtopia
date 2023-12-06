ecb29dc4dfcc0b9ec20978f9a3662e8db858daed
ysergma <sil3nthilldz@gmail.com>
2023-12-07 00:08:24 +0100

    add API GET,PATCH,DELETE  endpoints and their functionalities

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -18,10 +18,24 @@ import Profile from "@components/Profile"
     if(session?.user.id) fetchPosts();
   }, [session?.user.id]);
     const router = useRouter()
-    const handleEdit=()=>{
+    const handleEdit=(post)=>{
+      router.push(`/update-prompt?id=${post._id}`)
 
     }
-    const handleDelete=()=>{
+    const handleDelete= async (post)=>{
+      const hasConfirmed= confirm('Are you sure you want to delete this prompt')
+      if(hasConfirmed){
+        try {
+          await fetch(`api/prompt/${post._id.toString()}`,
+          {method:'DELETE',
+        }
+          )
+          const filteredPosts= posts.filter((p)=>p._id !== post._id)
+          setPosts(filteredPosts)
+        } catch (error) {
+          
+        }
+      }
 
     }
   return (
